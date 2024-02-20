2705d1d130fe75668f8ad809caa205641350c5fb
Hugo Piroux <84953759+hugodev-teck@users.noreply.github.com>
2024-02-20 09:00:31 +0100

    Add files via upload

---
+++ database_coscript/info/serachcodeinfo.js
@@ -0,0 +1,227 @@
+const jsonDataGen = [
+    {
+      "image": "https://upload.wikimedia.org/wikipedia/commons/0/0a/Python.svg" ,
+      "title": "Python",
+      "description": "Python (prononcé /pi.tɔ̃/) est un langage de programmation interprété, multiparadigme et multiplateformes. Il favorise la programmation impérative structurée, fonctionnelle et orientée objet. Il est doté d'un typage dynamique fort, d'une gestion automatique de la mémoire par ramasse-miettes et d'un système de gestion d'exceptions",
+      "categorie": "Langague de programmation",
+      "website": "https://www.python.org",
+      "source": "https://fr.wikipedia.org/wiki/Python_(langage)",
+      "sourcename": "Wikipedia"
+    },
+    {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/280px-C_Programming_Language.svg.png" ,
+        "title": "C",
+        "description": "C est un langage de programmation impératif, généraliste et de bas niveau. Inventé au début des années 1970 pour réécrire Unix, C est devenu un des langages les plus utilisés, encore de nos jours. De nombreux langages plus modernes comme C++, C#, Java et PHP ou JavaScript ont repris une syntaxe similaire au C",
+        "categorie": "Langague de programmation",
+        "website": "https://www.open-std.org/jtc1/sc22/wg14/",
+        "source": "https://https://fr.wikipedia.org/wiki/C_(langage)",
+        "sourcename": "Wikipedia"
+    },
+    {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/800px-ISO_C%2B%2B_Logo.svg.png" ,
+        "title": "C++",
+        "description": "C++ est un langage de programmation compilé permettant la programmation sous de multiples paradigmes, dont la programmation procédurale, la programmation orientée objet et la programmation générique. Ses bonnes performances, et sa compatibilité avec le langage C en font un des langages de programmation les plus utilisés dans les applications où la performance est critique.",
+        "categorie": "Langague de programmation",
+        "website": "https://isocpp.org/",
+        "source": "https://fr.wikipedia.org/wiki/C%2B%2B",
+        "sourcename": "Wikipedia"
+    },
+    {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/131px-Unofficial_JavaScript_logo_2.svg.png",
+        "title": "JavaScript",
+        "description": "JavaScript est un langage de programmation de scripts principalement utilisé dans les pages web interactives et côté client.",
+        "categorie": "Langage de programmation",
+        "website": "https://developer.mozilla.org/fr/docs/Web/JavaScript",
+        "source": "https://fr.wikipedia.org/wiki/JavaScript",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://www.svgrepo.com/download/75601/java-logo.svg",
+        "title": "Java",
+        "description": "Java est un langage de programmation orienté objet, concurrent et basé sur les classes.",
+        "categorie": "Langage de programmation",
+        "website": "https://www.oracle.com/java/",
+        "source": "https://fr.wikipedia.org/wiki/Java_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1280px-Ruby_logo.svg.png",
+        "title": "Ruby",
+        "description": "Ruby est un langage de programmation interprété, réflexif et objet.",
+        "categorie": "Langage de programmation",
+        "website": "https://www.ruby-lang.org/fr/",
+        "source": "https://fr.wikipedia.org/wiki/Ruby",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/0/0d/C_Sharp_wordmark.svg",
+        "title": "C#",
+        "description": "C# est un langage de programmation multi-paradigme développé par Microsoft.",
+        "categorie": "Langage de programmation",
+        "website": "https://docs.microsoft.com/en-us/dotnet/csharp/",
+        "source": "https://fr.wikipedia.org/wiki/C_Sharp",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png",
+        "title": "HTML",
+        "description": "HTML (HyperText Markup Language) est un langage de balisage utilisé pour créer la structure des pages web.",
+        "categorie": "Langage de balisage",
+        "website": "https://developer.mozilla.org/fr/docs/Web/HTML",
+        "source": "https://fr.wikipedia.org/wiki/HyperText_Markup_Language",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        "title": "CSS",
+        "description": "CSS (Cascading Style Sheets) est un langage de style utilisé pour définir la présentation des pages web écrites en HTML.",
+        "categorie": "Langage de style",
+        "website": "https://developer.mozilla.org/fr/docs/Web/CSS",
+        "source": "https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2048px-PHP-logo.svg.png",
+        "title": "PHP",
+        "description": "PHP (Hypertext Preprocessor) est un langage de script côté serveur, principalement utilisé pour le développement web dynamique.",
+        "categorie": "Langage de script",
+        "website": "https://www.php.net/",
+        "source": "https://fr.wikipedia.org/wiki/PHP",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Swift_logo.svg/1200px-Swift_logo.svg.png",
+        "title": "Swift",
+        "description": "Swift est un langage de programmation développé par Apple pour créer des applications iOS, macOS, watchOS et tvOS.",
+        "categorie": "Langage de programmation",
+        "website": "https://developer.apple.com/swift/",
+        "source": "https://fr.wikipedia.org/wiki/Swift_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+        "title": "TypeScript",
+        "description": "TypeScript est un sur-ensemble de JavaScript qui ajoute des fonctionnalités de typage statique.",
+        "categorie": "Langage de programmation",
+        "website": "https://www.typescriptlang.org/",
+        "source": "https://fr.wikipedia.org/wiki/TypeScript",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Go_Logo_Blue.svg/1280px-Go_Logo_Blue.svg.png",
+        "title": "Go",
+        "description": "Go (Golang) est un langage de programmation développé par Google, apprécié pour sa simplicité et son efficacité.",
+        "categorie": "Langage de programmation",
+        "website": "https://golang.org/",
+        "source": "https://fr.wikipedia.org/wiki/Go_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Kotlin_Icon_2021.svg/640px-Kotlin_Icon_2021.svg.png",
+        "title": "Kotlin",
+        "description": "Kotlin est un langage de programmation statiquement typé qui s'exécute sur la machine virtuelle Java (JVM) et peut être utilisé partout où Java est utilisé.",
+        "categorie": "Langage de programmation",
+        "website": "https://kotlinlang.org/",
+        "source": "https://fr.wikipedia.org/wiki/Kotlin_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Rust_programming_language_black_logo.svg/1920px-Rust_programming_language_black_logo.svg.png",
+        "title": "Rust",
+        "description": "Rust est un langage de programmation système qui se concentre sur la sécurité, la performance et la concurrence.",
+        "categorie": "Langage de programmation",
+        "website": "https://www.rust-lang.org/",
+        "source": "https://fr.wikipedia.org/wiki/Rust_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Scala-full-color.svg/1200px-Scala-full-color.svg.png",
+        "title": "Scala",
+        "description": "Scala est un langage de programmation multi-paradigme conçu pour exprimer les modèles de programmation courants de manière concise, élégante et avec des types sûrs.",
+        "categorie": "Langage de programmation",
+        "website": "https://www.scala-lang.org/",
+        "source": "https://fr.wikipedia.org/wiki/Scala_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png",
+        "title": "Dart",
+        "description": "Dart est un langage de programmation développé par Google, principalement utilisé pour le développement d'applications mobiles avec Flutter.",
+        "categorie": "Langage de programmation",
+        "website": "https://dart.dev/",
+        "source": "https://fr.wikipedia.org/wiki/Dart_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Lua-Logo.svg/1200px-Lua-Logo.svg.png",
+        "title": "Lua",
+        "description": "Lua est un langage de script utilisé dans un large éventail d'applications, notamment les jeux vidéo.",
+        "categorie": "Langage de script",
+        "website": "https://www.lua.org/",
+        "source": "https://fr.wikipedia.org/wiki/Lua_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Haskell-Logo.svg/1200px-Haskell-Logo.svg.png",
+        "title": "Haskell",
+        "description": "Haskell est un langage de programmation fonctionnel pur, avec une syntaxe élégante et des outils puissants pour écrire des logiciels robustes et concis.",
+        "categorie": "Langage de programmation",
+        "website": "https://www.haskell.org/",
+        "source": "https://fr.wikipedia.org/wiki/Haskell_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://elixir-lang.org/images/logo/logo.png",
+        "title": "Elixir",
+        "description": "Elixir est un langage de programmation fonctionnel et concurrent basé sur Erlang, conçu pour le développement scalable de systèmes distribués et de web applications robustes.",
+        "categorie": "Langage de programmation",
+        "website": "https://elixir-lang.org/",
+        "source": "https://fr.wikipedia.org/wiki/Elixir_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://upload.wikimedia.org/wikipedia/commons/8/81/OCaml_logo.svg",
+        "title": "OCaml",
+        "description": "OCaml est un langage de programmation fonctionnel avec des extensions pour la programmation impérative, développé dans le cadre du projet de recherche CAML à l'INRIA.",
+        "categorie": "Langage de programmation",
+        "website": "https://ocaml.org/",
+        "source": "https://fr.wikipedia.org/wiki/OCaml",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://fsharp.org/img/logo/fsharp256.png",
+        "title": "F#",
+        "description": "F# est un langage de programmation fonctionnel multi-paradigme fortement typé, conçu pour la programmation sur la plate-forme .NET de Microsoft.",
+        "categorie": "Langage de programmation",
+        "website": "https://fsharp.org/",
+        "source": "https://fr.wikipedia.org/wiki/F_Sharp_(langage)",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://th.bing.com/th/id/R.5f01b5b4299adbbc74135774a58e0ae7?rik=i%2fVUar9T9dKFqA&riu=http%3a%2f%2fwww.newthinktank.com%2fwp-content%2fuploads%2f2020%2f04%2fCOBOL-Tutorial.png&ehk=7LTc%2b2pmJVK3vHEDFjMnIi6y4V2YEFiBqeSO7%2bwQEU4%3d&risl=&pid=ImgRaw&r=0",
+        "title": "COBOL",
+        "description": "COBOL (Common Business-Oriented Language) est un langage de programmation utilisé principalement dans le domaine des applications d'entreprise pour les systèmes de gestion de bases de données et les transactions commerciales.",
+        "categorie": "Langage de programmation",
+        "website": "https://fr.wikipedia.org/wiki/COBOL",
+        "source": "https://fr.wikipedia.org/wiki/COBOL",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://i.pinimg.com/originals/8c/b1/8c/8cb18c72082d13eb581cf6d452e8e266.png",
+        "title": "Assembleur",
+        "description": "L'assembleur est un langage de bas niveau utilisé pour programmer directement les microprocesseurs et les microcontrôleurs. Il permet un contrôle précis sur le matériel informatique mais nécessite une connaissance approfondie de l'architecture du processeur.",
+        "categorie": "Langage de programmation",
+        "website": "https://en.wikipedia.org/wiki/X86_assembly_language",
+        "source": "https://en.wikipedia.org/wiki/X86_assembly_language",
+        "sourcename": "Wikipedia"
+      },
+      {
+        "image": "https://th.bing.com/th/id/OIP.JQmDFSkFj1znxu8wXKZfWAHaHa?rs=1&pid=ImgDetMain",
+        "title": "BASIC",
+        "description": "BASIC (Beginner's All-purpose Symbolic Instruction Code) est un langage de programmation de haut niveau conçu pour être simple à apprendre et à utiliser. Il a été largement utilisé dans les années 1970 et 1980 pour l'enseignement de la programmation et le développement de logiciels sur les micro-ordinateurs.",
+        "categorie": "Langage de programmation",
+        "website": "https://fr.wikipedia.org/wiki/BASIC",
+        "source": "https://fr.wikipedia.org/wiki/BASIC",
+        "sourcename": "Wikipedia"
+      }
+    ];
